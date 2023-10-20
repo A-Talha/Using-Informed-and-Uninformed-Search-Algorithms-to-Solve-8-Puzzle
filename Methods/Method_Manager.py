064816ca5fd02b6cc5e364e1f36c00b019502b70
MostafaGalal1 <mostafag649.mg@gmail.com>
2023-10-20 18:13:25 +0300

    Manhattan_Heuristic.py finished added some common functions to Method_Body.py setters and getters for solution class time precision done

--- Methods/Method_Manager.py
+++ Methods/Method_Manager.py
@@ -4,16 +4,16 @@ from Manhattan_Heuristic import *
 from Euclidean_Heuristic import *
 
 
-def manger(starting_state,method):
+def manager(initial_state, method):
     match method:
         case "BFS":
-            return BFS(starting_state)
+            return BFS(initial_state)
         case "DFS":
-            return DFS(starting_state)
+            return DFS(initial_state)
         case "Manhattan":
-            return Manhattan(starting_state)
+            return Manhattan(initial_state)
         case "Euclidean":
-            return Euclidean(starting_state)
+            return Euclidean(initial_state)
         case _:
             pass
 

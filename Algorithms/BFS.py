d08f870e518788c2e05fea98604332ca56a395b7
A-Talha <96351797+A-Talha@users.noreply.github.com>
2023-10-24 08:16:07 +0300

    optimised code

--- Algorithms/BFS.py
+++ Algorithms/BFS.py
@@ -10,58 +10,43 @@ class BFS(Search_Algorithm):
 
     def solve(self):
         if not self.is_solvable():
-            print(self.is_solvable())
             return Solution(False)
 
         frontier = queue.Queue()
-        initial_state_str = self.stringify_state_dfs_and_bfs(self.initial_state)
-        goal_state_str = self.stringify_state_dfs_and_bfs(self.goal_test)
-        frontier.put(initial_state_str)
         explored = set()
-        all = set()
-        parent = dict()          # Parent map its key : current_state & value : (cost of the current_state, its parent)
+        parent = dict()  # Parent map its key : current_state & value : (cost of the current_state, its parent)
 
+        frontier.put(self.initial_state)
+        parent[self.initial_state] = (0, None)
         search_depth = 0
 
-
-        parent[initial_state_str] = (0, None)
-
-        start_time = time.perf_counter()
+        start_time = time.time()
 
         while not frontier.empty():
-            current_state_str = frontier.get()
-            explored.add(current_state_str)
-            all.add(current_state_str)
+            current_state = frontier.get()
+            explored.add(current_state)
 
-            if current_state_str == goal_state_str:
+            if current_state == self.goal_test:
                 break
 
-            cost = parent[current_state_str][0]
+            cost = parent[current_state][0]
             cost += 1
 
-            x, y, zero = self.get_empty_tile_location_dfs_and_bfs(current_state_str)
-
+            x, y = self.get_empty_tile_location(current_state)
             for move in range(4):
                 if not self.is_valid_move(x, y, move):
                     continue
-             #   print(move, "jgf",current_state_str)
 
+                new_state = self.apply_move(current_state, x, y, move)
+                if new_state not in explored and new_state not in parent:
+                    frontier.put(new_state)
+                    parent[new_state] = (cost, current_state)
+                    search_depth = max(search_depth, cost)
 
-                new_state_str = self.apply_move_bfs_and_dfs(current_state_str, zero, move)
-              #  print(new_state_str)
+        end_time = time.time()
 
-                if new_state_str not in explored and new_state_str not in all:
-                    all.add(new_state_str)
-                    frontier.put(new_state_str)
-                    parent[new_state_str] = (cost, current_state_str)
-                    search_depth = max(search_depth , cost)
-
-        end_time = time.perf_counter()
-        print(end_time - start_time)
-        path = self.find_path(self.goal_test, parent, True)
-        total_time = end_time - start_time
-        cost = parent[goal_state_str][0]
+        path = self.find_path(parent)
+        cost = parent[self.goal_test][0]
         nodes_expanded = len(explored)
-        return Solution(True, path, cost, nodes_expanded, search_depth, total_time)
-
-
+        running_time = end_time - start_time
+        return Solution(True, path, cost, nodes_expanded, search_depth, running_time)

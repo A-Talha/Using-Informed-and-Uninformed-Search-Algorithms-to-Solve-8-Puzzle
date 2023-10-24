d6de43763c42136065aacca27245d9c94fad59c6
mohamed kamal <kamalmohammed837@gmail.com>
2023-10-24 14:59:04 +0300

    more tests

--- Test/Test.py
+++ Test/Test.py
@@ -52,9 +52,7 @@ class Test(unittest.TestCase):
             [[1, 4, 2], [6, 5, 8], [7, 3, 0]],
             [[1, 0, 2], [7, 5, 4], [8, 6, 3]],
         ]
-        initial_states1 = [
 
-        ]
         for i, initial_state in enumerate(initial_states, 1):
             print(f'\n######## {i} ########')
             self.Solvable(initial_state)
@@ -63,14 +61,92 @@ class Test(unittest.TestCase):
         initial_states = [
             [[1, 2, 3], [4, 5, 6], [8, 7, 0]],
             [[8, 1, 2], [0, 4, 3], [7, 6, 5]],
-            [[2, 3, 1], [0, 5, 4],[7, 8, 6]],
+            [[2, 3, 1], [0, 5, 4], [7, 8, 6]],
             [[1, 8, 2], [4, 5, 3], [7, 6, 0]],
-
         ]
         for initial_state in initial_states:
             for algorithm, heuristic in self.algorithms:
                 self.notSolvable(initial_state, algorithm, heuristic)
 
+    def test_Manhattan1(self):
+        puzzle = get_algorithm([[1, 2, 5], [3, 4, 0], [6, 7, 8]], "AStar", "Manhattan")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 3)
+        self.assertEqual(solution.nodes_expanded, 4)
+        self.assertEqual(solution.search_depth, 3)
+
+    def test_Manhattan2(self):
+        puzzle = get_algorithm([[1, 4, 2], [6, 5, 8], [7, 3, 0]], "AStar", "Manhattan")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 8)
+        self.assertEqual(solution.nodes_expanded, 11)
+        self.assertEqual(solution.search_depth, 8)
+
+    def test_Manhattan3(self):
+        puzzle = get_algorithm([[1, 2, 5], [6, 3, 0], [4, 7, 8]], "AStar", "Manhattan")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 11)
+        self.assertEqual(solution.nodes_expanded, 24)
+        self.assertEqual(solution.search_depth, 11)
+
+    def test_Euclidean1(self):
+        puzzle = get_algorithm([[1, 2, 5], [3, 4, 0], [6, 7, 8]], "AStar", "Euclidean")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 3)
+        self.assertEqual(solution.nodes_expanded, 4)
+        self.assertEqual(solution.search_depth, 3)
+
+    def test_Euclidean2(self):
+        puzzle = get_algorithm([[1, 4, 2], [6, 5, 8], [7, 3, 0]], "AStar", "Euclidean")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 8)
+        self.assertEqual(solution.nodes_expanded, 13)
+        self.assertEqual(solution.search_depth, 8)
+
+    def test_Euclidean3(self):
+        puzzle = get_algorithm([[1, 2, 5], [6, 3, 0], [4, 7, 8]], "AStar", "Euclidean")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 11)
+        self.assertEqual(solution.nodes_expanded, 54)
+        self.assertEqual(solution.search_depth, 11)
+
+    def test_DFS1(self):
+        puzzle = get_algorithm([[3, 1, 2], [0, 4, 5], [6, 7, 8]], "DFS")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 1)
+        self.assertEqual(solution.nodes_expanded, 2)
+        self.assertEqual(solution.search_depth, 1)
+
+    def test_DFS2(self):
+        puzzle = get_algorithm(  [[1, 2, 5], [3, 4, 0], [6, 7, 8]], "DFS")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 27)
+        self.assertEqual(solution.nodes_expanded, 28)
+        self.assertEqual(solution.search_depth, 27)
+
+    def test_BFS1(self):
+        puzzle = get_algorithm([[3, 1, 2], [0, 4, 5], [6, 7, 8]], "BFS")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 1)
+        self.assertEqual(solution.nodes_expanded, 4)
+        self.assertEqual(solution.search_depth, 2)
+
+    def test_BFS2(self):
+        puzzle = get_algorithm([[1, 2, 5], [3, 4, 0], [6, 7, 8]], "BFS")
+        solution = puzzle.solve()
+        self.assertTrue(solution.solvable)
+        self.assertEqual(solution.cost, 3)
+        self.assertEqual(solution.nodes_expanded, 13)
+        self.assertEqual(solution.search_depth, 4)
 
 
 if __name__ == '__main__':
